0585dcb3838591ac15c4df794621b228a30bd668
BryanGordon <bryanbge92@gmail.com>
2024-04-11 18:18:01 -0500

    Update App.tsx
    - Refactorized code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 import './App.css'
-import { useReducer } from 'react'
+import { Container, Row, Col, Button, Stack } from 'react-bootstrap'
 
-const initialState = {
-  fromLanguage: 'auto',
-  toLanguage: 'en',
-  fromText: '',
-  result: '',
-  loading: 'false'
-}
-
-function reducer (state, action) {
-
-}
+import { useStore } from './hooks/useStore'
+import { AUTO_LANGUAGES } from './constants'
+import { ArrowIcon } from './components/Icons'
+import { LanguageSelector } from './components/LanguageSelector'
+import { TextArea } from './components/TextArea'
+import { SectionType } from './types.d'
 
 function App () {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const { fromLanguage, toLanguage, fromText, result, interchangeLanguages, setFromLanguage, setToLanguage, setFromText, setResult, loading } = useStore()
   return (
-    <>
+    <Container fluid>
       <h1>Translate</h1>
-    </>
+      <Row>
+        <Col>
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.From}
+              value={fromLanguage}
+              onChange={setFromLanguage}
+            />
+            <TextArea
+              type={SectionType.From}
+              value={fromText}
+              onChange={setFromText}
+            />
+          </Stack>
+        </Col>
+
+        <Col xs='auto'>
+          <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGES} onClick={interchangeLanguages}>
+            <ArrowIcon />
+          </Button>
+        </Col>
+
+        <Col>
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.To}
+              value={toLanguage}
+              onChange={setToLanguage}
+            />
+            <TextArea
+              loading={loading}
+              type={SectionType.To}
+              value={result}
+              onChange={setResult}
+            />
+          </Stack>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
