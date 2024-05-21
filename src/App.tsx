e8f59d01e1da0614d388eb1c1f707494de20b91c
BryanGordon <bryanbge92@gmail.com>
2024-05-20 19:55:55 -0500

    Add translate functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,23 @@ import { ArrowIcon } from './components/Icons'
 import { LanguageSelector } from './components/LanguageSelector'
 import { TextArea } from './components/TextArea'
 import { SectionType } from './types.d'
+import { useEffect } from 'react'
+import { translate } from './services/translate'
 
 function App () {
   const { fromLanguage, toLanguage, fromText, result, interchangeLanguages, setFromLanguage, setToLanguage, setFromText, setResult, loading } = useStore()
+
+  useEffect(() => {
+    if (fromText === '') return
+
+    translate({ fromLanguage, toLanguage, text: fromText })
+      .then(result => {
+        if (result == null) return
+        setResult(result)
+      })
+      .catch(() => { setResult('Error') })
+  }, [fromText])
+
   return (
     <Container fluid>
       <h1>Translate</h1>
