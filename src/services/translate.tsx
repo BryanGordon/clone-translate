fc557f9fd88a7d122a868acbcf0f6aec7dd6734c
BryanGordon <bryanbge92@gmail.com>
2024-05-20 19:56:31 -0500

    Configure openai conexion

--- src/services/translate.tsx
+++ src/services/translate.tsx
@@ -2,9 +2,8 @@ import { ChatCompletionRequestMessageRoleEnum, Configuration, OpenAIApi } from '
 import { SUPPORTED_LANGUAGED } from '../constants'
 import { FromLanguage, Language } from '../types'
 
-const apikey = import.meta.env.VITE_APIKEY
-
-const configuration = new Configuration(apikey)
+const apiKey = import.meta.env.VITE_APIKEY
+const configuration = new Configuration({ apiKey })
 const openai = new OpenAIApi(configuration)
 
 export async function translate ({
@@ -16,10 +15,52 @@ export async function translate ({
   toLanguage: Language,
   text: string
 }) {
+  if (fromLanguage === toLanguage) return text
+
   const messages = [
     {
       role: ChatCompletionRequestMessageRoleEnum.System,
-      content: ''
+      content: 'You are a AI that only translate text. You will recieve a text from the user you do not answer that, just translate the text. The original language is surrounded by `{{` and `}}`. Also you can recieve {{auto}} which means that you have to detect the language. The language you translate to us surrounded by `[[` and `]]`.'
+    },
+    {
+      role: ChatCompletionRequestMessageRoleEnum.User,
+      content: 'Hola usuario {{Spanish}} [[English]]'
+    },
+    {
+      role: ChatCompletionRequestMessageRoleEnum.Assistant,
+      content: 'Hello user'
+    },
+    {
+      role: ChatCompletionRequestMessageRoleEnum.User,
+      content: 'How are you? {{English}} [[Deutsch]]'
+    },
+    {
+      role: ChatCompletionRequestMessageRoleEnum.Assistant,
+      content: 'Wie geht es dir?'
+    },
+    {
+      role: ChatCompletionRequestMessageRoleEnum.User,
+      content: 'Bom dia, com estas? {{auto}} [[Spanish]]'
+    },
+    {
+      role: ChatCompletionRequestMessageRoleEnum.Assistant,
+      content: 'Buenos días, ¿cómo estás?'
     }
   ]
+
+  const fromCode = fromLanguage === 'auto' ? 'auto' : SUPPORTED_LANGUAGED[fromLanguage]
+  const toCode = SUPPORTED_LANGUAGED[toLanguage]
+
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      ...messages,
+      {
+        role: ChatCompletionRequestMessageRoleEnum.User,
+        content: `${text} {{${fromCode}}} [[${toCode}]]`
+      }
+    ]
+  })
+
+  return completion.data.choices[0]?.message?.content
 }
